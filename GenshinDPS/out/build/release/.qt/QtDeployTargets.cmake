1e7d2f54ded04bed71b9927db06b655f9db10b6a
Yanzhang Tong <2024302141145@whu.edu.cn>
2026-04-30 22:15:16 +0800

    Delete unnecessary files for cloning

--- GenshinDPS/out/build/release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_GenshinDPS_FILE E:/桌面/DPS calculater/DeepSeekV4 ver/GenshinDPS/out/build/release/GenshinDPS.exe)
-set(__QT_DEPLOY_TARGET_GenshinDPS_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_GenshinDPS_RUNTIME_DLLS E:/Qt/6.11.0/msvc2022_64/bin/Qt6Widgets.dll;E:/Qt/6.11.0/msvc2022_64/bin/Qt6Gui.dll;E:/Qt/6.11.0/msvc2022_64/bin/Qt6Core.dll)
